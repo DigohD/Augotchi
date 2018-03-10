cc2e29cb9e3253ab26a617f697f7bee55b579921
Simon Eliasson <digohd1337@gmail.com>
2018-03-10 11:27:44 +0100

    Some Audio Stuff

---
+++ Augotchi/Assets/Augotchi/UI/MusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (GameObject.FindGameObjectsWithTag("MusicPlayer").Length > 1)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+	}
+}
