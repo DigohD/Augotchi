36e27d7bab8ddd09c58f247e57f9c8fda9249231
Simon Eliasson <digohd1337@gmail.com>
2018-03-12 10:23:48 +0100

    New MArkers

---
+++ Augotchi/Assets/Augotchi/Util/SinusWave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinusWave : MonoBehaviour {
+
+    public float amplitude;
+    public float frequency;
+    public float offset;
+
+    public float timer;
+
+    private Vector3 origin;
+
+    private void Start()
+    {
+        origin = transform.localPosition;
+    }
+
+    void Update () {
+        timer += Time.deltaTime;
+        transform.localPosition = origin + amplitude * (Vector3.forward * Mathf.Sin((frequency * timer * 6.28f) + (offset * 6.28f)));
+	}
+}
---
+++ Augotchi/Assets/Mapbox/Unity/Map/RotateObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    public float speed;
+
+	void Update () {
+        transform.RotateAroundLocal(Vector3.up, speed * Time.deltaTime);
+	}
+}
