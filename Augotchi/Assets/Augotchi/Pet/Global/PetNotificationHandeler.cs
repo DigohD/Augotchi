93a7e4042a374f979d0f50d7e96f18ebab72d07a
Kevin Björklund <duappe93@gmail.com>
2018-03-27 15:54:06 +0200

    fixed plan notification method

--- Augotchi/Assets/Augotchi/Pet/Global/PetNotificationHandeler.cs
+++ Augotchi/Assets/Augotchi/Pet/Global/PetNotificationHandeler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.SimpleAndroidNotifications;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,7 +18,42 @@ public class PetNotificationHandeler : MonoBehaviour {
 
     public static void PlanNotification(PetGlobal pg)
     {
-        
+        int whatNotificationToSend = 0; // 1= Hunger, 2= happiness, 3=Health
+
+        TimeSpan smallestTimespan = CalculateHungerTime(pg.hunger);
+        whatNotificationToSend = 1;
+
+        if(CalculateHappinessTime(pg.happiness, pg.hunger, pg.health) < smallestTimespan)
+        {
+            smallestTimespan = CalculateHappinessTime(pg.happiness, pg.hunger, pg.health);
+            whatNotificationToSend = 2;
+        }
+        if(CalculateHealthTime(pg.health) < smallestTimespan)
+        {
+            smallestTimespan = CalculateHealthTime(pg.health);
+            whatNotificationToSend = 3;
+        }
+
+        //Determined what notification to send, following is sending the notification:
+
+        if (whatNotificationToSend == 1)
+        {
+            NotificationManager.SendWithAppIcon(smallestTimespan, "Augotchi hunger", "Your pet is literary starving rn", new Color(1f,0f,0f), NotificationIcon.Clock);
+        }
+        else if (whatNotificationToSend == 2)
+        {
+            NotificationManager.SendWithAppIcon(smallestTimespan, "Augotchi happiness", "Your pet is literary suicidal rn", new Color(1f, 0f, 0f), NotificationIcon.Clock);
+        }
+        else if (whatNotificationToSend == 3)
+        {
+            NotificationManager.SendWithAppIcon(smallestTimespan, "Augotchi health", "Your pet is literary drowning in fat rn", new Color(1f, 0f, 0f), NotificationIcon.Clock);
+        }
+        else
+        {
+            Debug.LogWarning("Something went horribly wrong with sending a notification! Could not determine smallest!");
+        }
+
+
 
     }
     public static TimeSpan CalculateHungerTime(float hungerValue)
